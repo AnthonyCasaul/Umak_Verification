a454f19a3ec3485078e71c6dec29f4c3f75cfe99
xianity <christianjosephcuerdo@gmail.com>
2023-06-15 02:02:06 +0800

    PDF
    konting update

---
+++ VerificationSystem/certificate_generate.php
@@ -0,0 +1,32 @@
+<?php
+require __DIR__ ."/vendor/autoload.php";
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+include('condb.php');
+session_start();
+
+
+
+
+
+
+
+$options = new Options;
+$options->set('isRemoteEnabled', true);
+$options->setChroot(__DIR__);
+
+$dompdf = new Dompdf($options);
+$dompdf->setPaper("A4", "portrait");
+
+$html = file_get_contents("certificate_template.html");
+$html = str_replace(["{{ name }}", "{{ date }}"], [$name, $date], $html);
+
+
+
+$dompdf->loadhtml($html);
+$dompdf->render();
+$dompdf->addInfo("Title","Alumni_Certification");
+$dompdf->stream("Alumni_Certification.pdf",["Attachment"=>0])
+
+?>
