4fe6ccc0b57b7df31b2295f2779256aac8390622
xianity <christianjosephcuerdo@gmail.com>
2023-06-20 16:07:42 +0800

    Update ddlYear.js

--- VerificationSystem/ddlYear.js
+++ VerificationSystem/ddlYear.js
@@ -1,19 +1,19 @@
 var currentYear = new Date().getFullYear();
-    
-    // Create the dropdown options
-    var dropdown = document.getElementById("yearDropdown");
 
-    // Add the preselected option
-    var preselectedOption = document.createElement("option");
-    preselectedOption.text = (currentYear - 1) + "-" + currentYear;
-    preselectedOption.value = (currentYear - 1) + "-" + currentYear;
-    preselectedOption.selected = true;
-    dropdown.add(preselectedOption);
+// Create the dropdown options
+var dropdown = document.getElementById("yearDropdown");
 
-    // Add the remaining options
-    for (var year = 1965; year <= currentYear; year++) {
-      var option = document.createElement("option");
-      option.text = year + "-" + (year + 1);
-      option.value = year + "-" + (year + 1);
-      dropdown.add(option);
-    }
+// Add the preselected option
+var preselectedOption = document.createElement("option");
+preselectedOption.text = currentYear - 1 + "-" + currentYear;
+preselectedOption.value = currentYear - 1 + "-" + currentYear;
+preselectedOption.selected = true;
+dropdown.add(preselectedOption);
+
+// Add the remaining options
+for (var year = 1965; year <= currentYear; year++) {
+  var option = document.createElement("option");
+  option.text = year + "-" + (year + 1);
+  option.value = year + "-" + (year + 1);
+  dropdown.add(option);
+}
